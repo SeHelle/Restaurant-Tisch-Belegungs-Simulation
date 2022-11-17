d5fb10f47b2b90277e22dec057bc9c6f5cf44238
SeHelle <js@helle.email>
2022-11-17 11:23:57 +0100

    start

---
+++ Restaurant-Tisch-Belegungs-Simulation/Restaurant-Tisch-Belegungs-Simulation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Restaurant_Tisch_Belegungs_Simulation
+{
+    class Program
+    {
+        public static Dictionary<string, int> FreieStuehleTisch = new Dictionary<string, int>();
+        public static Dictionary<string, string> GruppeTisch = new Dictionary<string, string>();
+        public static Dictionary<string, int> PersonenGruppe = new Dictionary<string, int>();
+        public static Dictionary<string, int> neueBelegung = new Dictionary<string, int>();
+        public static List<string> TischeMitBelgung = new List<string>();
+
+        static void Main(string[] args)
+        {
+
+            var restaurant = new Restaurant().getStuehle();
+            new Gruppen().getPersonen();
+
+            Console.WriteLine("Willkommen in unserem Restaurant! Heute haben wir "+restaurant.Count+" Stühle frei.");
+            Console.WriteLine("Da sind schon die ersten Gruppen, heute sind es " + PersonenGruppe.Count + " Gruppen welche wir gerne beköstigen.");
+            new Tischzuweisung().freienTischfinden();
+            new Verabschieden().stuehleFreigeben();
+        }
+    }
+
+
+}
