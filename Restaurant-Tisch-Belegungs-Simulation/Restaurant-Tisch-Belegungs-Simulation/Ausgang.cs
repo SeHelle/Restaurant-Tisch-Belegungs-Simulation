c61c71c4205f8235d5e54b20cde4b0455f0e29a5
SeHelle <js@helle.email>
2022-11-17 19:09:11 +0100

    update ausgang.cs

--- Restaurant-Tisch-Belegungs-Simulation/Restaurant-Tisch-Belegungs-Simulation/Ausgang.cs
+++ Restaurant-Tisch-Belegungs-Simulation/Restaurant-Tisch-Belegungs-Simulation/Ausgang.cs
@@ -12,9 +12,10 @@ namespace Restaurant_Tisch_Belegungs_Simulation
                 if (GruppeTisch.ContainsKey("Tisch" + stuehle))
                 {
                     Console.WriteLine("Leider verlässt uns" + GruppeTisch["Tisch"+stuehle]);
-                    
 
-                    FreieStuehleTisch["Tisch" + stuehle] = alteBelegung["Tisch" + stuehle];
+                    int PersonenGruppe = MappingGruppen[GruppeTisch["Tisch" + stuehle]];
+                    int NochFreieStuehle = neueBelegung["Tisch" + stuehle];
+                    FreieStuehleTisch["Tisch" + stuehle] = NochFreieStuehle + PersonenGruppe;
                     GruppeTisch.Remove("Tisch" + stuehle);
 
                     }
